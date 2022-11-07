d4405e4c84e5eb4259de564f72e58af81652bcb2
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-07 19:00:22 +0900

    feat(User): 사용자 값 1~9로 이루어진 수인지 확인

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -8,6 +8,8 @@ import java.util.List;
 public class User {
     private static final String INPUT_MASSAGE = "숫자를 입력해주세요 : ";
     private static final int NUMBER_LENGTH = 3;
+    private static final int FIRST_NUM = 1;
+    private static final int LAST_NUM = 9;
 
     public List<Integer> getUserNumber() {
         List<Integer> userNumber = new ArrayList<>();
@@ -23,5 +25,16 @@ public class User {
 
     public boolean checkLength(List<Integer> num) {
         return num.size() == NUMBER_LENGTH;
+    }
+
+    public boolean checkRange(List<Integer> num) {
+        int check = 0;
+
+        for (int i = 0; i <3; i++) {
+            if(FIRST_NUM <= num.get(i) && num.get(i) <= LAST_NUM) {
+                check += 1;
+            }
+        }
+        return check == NUMBER_LENGTH;
     }
 }
